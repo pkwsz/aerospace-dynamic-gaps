324e8d9f80369e048836690b487869fccfa8bf67
Patryk Kowejsza <patryk@kowejsza.de>
2026-05-07 19:32:36 +0200

    overwrite gaps on threshhold
    fixed gap values caused weird behaviour on regular 16:9 aspect ratio screens
    to fix it the aspect ratio threshhold has been set to 3.5:1 which rougly corresponds to 32:9

--- dynamic-gaps.sh
+++ dynamic-gaps.sh
@@ -7,6 +7,16 @@ set_outer_gap() {
 	sed -i '' -E "s/^([[:space:]]*outer\.right[[:space:]]*=[[:space:]]*)[0-9]+/\1$1/" "$config_path"
 }
 
+resolution=(`system_profiler -json SPDisplaysDataType | grep '"_spdisplays_resolution"' | grep -oE '[0-9]+'`)
+width=${resolution[0]}
+height=${resolution[1]}
+is_not_wide_enough=$(echo "$width / $height < 3.5" | bc -l)
+
+if [ $is_not_wide_enough ]; then
+	set_outer_gap "$gap"
+	exit 0	
+fi
+
 window_count=$(aerospace list-windows --workspace focused | wc -l) 
 if [ $window_count -gt 1 ]; then
 	set_outer_gap "$gap"
